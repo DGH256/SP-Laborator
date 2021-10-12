a12a6a3a9504090749c3ac8c2aafee368672229e
DorinGalben <91731551+DorinGalben@users.noreply.github.com>
2021-10-12 15:43:17 +0300

    Laborator 3

---
+++ SP-Lab3/src/com/company/Table.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Table extends Element //Everything is an element now!
+{
+    public Table(String title) {
+       this.setTitle(title);
+    }
+}
